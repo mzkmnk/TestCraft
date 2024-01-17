33ed3210515e1b6a68d0ff90765594f423aff097
Nishio0228 <tky2202155@stu.o-hara.ac.jp>
2024-01-17 12:35:11 +0900

    簡易的な採点処理の追加

--- frontend/app/src/QuestionsSolve/Result.js
+++ frontend/app/src/QuestionsSolve/Result.js
@@ -1,3 +1,40 @@
 export default function Result({ answers, questionTree, questionIds }) {
-  return <h1>Result</h1>;
+  const correctIds = [];
+  // rootを除く
+  questionIds = questionIds.slice(1);
+
+  // 正答判定
+  const grade = (questionIds) => {
+    for (const id of questionIds) {
+      // nestedの場合は再帰
+      if (questionTree[id].questionType === "nested") {
+        grade(questionTree[id].childIds);
+        continue;
+      }
+
+      const correctObjArray = questionTree[id].answers;
+      console.log(questionTree[id]);
+      const answer = answers[id];
+      if (answer === undefined) {
+        continue;
+      }
+      for (const correctObj of correctObjArray) {
+        if (correctObj.value === answer) {
+          correctIds.push(id);
+          break;
+        }
+      }
+    }
+  };
+
+  grade(questionIds);
+
+  return (
+    <>
+      <h1>Result</h1>
+      <p>
+        正答率：{correctIds.length} ／ {questionIds.length}
+      </p>
+    </>
+  );
 }
