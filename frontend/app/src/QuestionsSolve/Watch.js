24cf5b2bda81c6ce9a453d2fd604c66f4e9e6fa8
Nishio0228 <tky2202155@stu.o-hara.ac.jp>
2024-01-19 11:08:39 +0900

    タイマーの位置を調整

--- frontend/app/src/QuestionsSolve/Watch.js
+++ frontend/app/src/QuestionsSolve/Watch.js
@@ -1,12 +1,14 @@
+import { Box, Typography } from "@mui/material";
+
 export default function Watch({ time }) {
   const minutes = Math.floor(time / 60);
   const seconds = time - minutes * 60;
 
   return (
-    <div>
-      <div>
+    <Box sx={{ marginTop: 10, position: "absolute", right: 10 }}>
+      <Typography>
         {minutes}分{seconds}秒
-      </div>
-    </div>
+      </Typography>
+    </Box>
   );
 }
