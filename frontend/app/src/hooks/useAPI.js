cc0c181f8cc9f7594489c054a826147b4fe9f3c0
HidemaruOwO <sma0729mex@gmail.com>
2024-01-26 23:35:07 +0900

    url変更

--- frontend/app/src/hooks/useAPI.js
+++ frontend/app/src/hooks/useAPI.js
@@ -1,7 +1,8 @@
 import { useState, useEffect, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 
-const urlBase = "https://54.150.14.102:8000/api";
+// const urlBase = "https://54.150.14.102:8000/api";
+const urlBase = "https://testcraft.ddns.net:8000/api";
 const APIs = {
   singup: `${urlBase}/signup`,
   login: `${urlBase}/login`,
