db421da7e5c26b46d70b1ee4c7fd822b5f1542ea
mzkmnk <sma0729mex@gmail.com>
2024-01-29 14:38:33 +0900

    urlBase変更

--- backend/project/project/settings.py
+++ backend/project/project/settings.py
@@ -50,9 +50,9 @@ INSTALLED_APPS = [
 NINJA_DOCS_VIEW = 'redoc'
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
--- frontend/app/src/hooks/useAPI.js
+++ frontend/app/src/hooks/useAPI.js
@@ -1,7 +1,7 @@
 import { useState, useEffect, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 
-const urlBase = "https://api.testcrafts.net/api";
+const urlBase = "https://api.testcrafts.net:8000/api";
 // const urlBase = "http://localhost:8000/api";
 const APIs = {
   singup: `${urlBase}/signup`,
