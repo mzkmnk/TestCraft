780db62273d520dae6ee8861f3454186e60c0a19
Nishio0228 <tky2202155@stu.o-hara.ac.jp>
2024-01-29 09:52:54 +0900

    AllComppanyUserをuseAPI化

--- frontend/app/src/AllCompanyUsers.js
+++ frontend/app/src/AllCompanyUsers.js
@@ -1,67 +1,48 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { DataGrid } from '@mui/x-data-grid';
-import UserHeader from './UserHeader';
+import { DataGrid } from "@mui/x-data-grid";
+import UserHeader from "./UserHeader";
+import { useAPI } from "./hooks/useAPI";
 
 function AllCompanyUsers() {
-    const navigate = useNavigate();
-    const [users, setUsers] = useState([]);
+  const navigate = useNavigate();
+  const [users, setUsers] = useState([]);
 
-    useEffect(() => {
-        fetch("http://localhost:8000/api/check_auth", {
-            headers: {
-                "Content-Type": "application/json",
-            },
-            credentials: "include",
-        })
-        .then((response) => response.json())
-        .then((data) => {
-            if(data.authenticated === false){
-                navigate("/login");
-            }else{
-                fetch("http://localhost:8000/api/all_company_users", {
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    credentials: "include",
-                })
-                .then((response) => response.json())
-                .then((data) => {
-                    if(data.success){
-                        setUsers(data.data);
-                    }else{
-                        console.error(data.error);
-                    }
-                })
-            }
-        })
-        .catch((error) => {
-            console.error("Error:", error);
-        });
-    }, [navigate]);
+  const API = useAPI({
+    APIName: "all_company_users",
+    isLoginRequired: "true",
+    loadOnStart: true,
+  });
 
+  useEffect(() => {
+    if (API.isSuccess === false) {
+      navigate("/error");
+    } else if (API.isSuccess === true && API.data.success === true) {
+      setUsers(API.data.data);
+    }
+  }, [API.data.success, API.data.workbook, API.isSuccess, navigate]);
 
-    const columns = [
-        { field: 'name', headerName: 'Name', width: 350 },
-        { field: 'created_at', headerName: 'Date Created', width: 150 },
-        //今後解く必要がある問題数を表示できたらいいな
-    ];
+  const columns = [
+    { field: "name", headerName: "Name", width: 350 },
+    { field: "created_at", headerName: "Date Created", width: 150 },
+    //今後解く必要がある問題数を表示できたらいいな
+  ];
 
-    return (
-        <>
-            <div style={{ marginBottom: '20px' }}>
-                <UserHeader />
-            </div>
-            <div style={{ height: '100% ', width: '100%' }}>
-                <DataGrid
-                    rows={users}
-                    columns={columns}
-                    pageSize={5}
-                    checkboxSelection
-                />
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div style={{ marginBottom: "20px" }}>
+        <UserHeader />
+      </div>
+      <div style={{ height: "100% ", width: "100%" }}>
+        <DataGrid
+          rows={users}
+          columns={columns}
+          pageSize={5}
+          checkboxSelection
+        />
+      </div>
+    </>
+  );
 }
 
 export default AllCompanyUsers;
