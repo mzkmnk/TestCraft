150335cec1bcce2f4f12efa61bc960b45bd8f980
mzkmnk <sma0729mex@gmail.com>
2024-01-30 21:44:42 +0900

    Refactor code to improve performance and readability

--- backend/project/project/urls.py
+++ backend/project/project/urls.py
@@ -14,13 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 # 変更
 
 # from api.api import *
-from api import api
+# from api import api
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', api.urls),
+    path('api/', include('api.urls')),
 ]
