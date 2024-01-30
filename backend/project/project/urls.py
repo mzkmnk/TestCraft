12b45fc5d6d56be321505461731c2dd7dd7f8e7d
mzkmnk <sma0729mex@gmail.com>
2024-01-30 10:56:28 +0900

    project/urls変更

--- backend/project/project/urls.py
+++ backend/project/project/urls.py
@@ -14,14 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 # 変更
-# from django.urls import include
 
 from api.api import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', api.urls),
-    # path('api/',include('api.api')),
+    path('',include('api.urls')),
 ]
