91ee24eedcc77a5751dee2f5c42e92155d2767a8
mzkmnk <sma0729mex@gmail.com>
2024-02-03 01:32:38 +0900

    settings.py env対応

--- backend/project/project/settings.py
+++ backend/project/project/settings.py
@@ -151,9 +151,9 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'testcrarts.official@gmail.com'
-EMAIL_HOST_PASSWORD = 'jcbg qokj lujm llbv'
-EMAIL_USE_TLS = True
+EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') == 'True'
